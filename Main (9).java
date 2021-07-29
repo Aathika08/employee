1082a2b8d1dc7568933144b964a46a854cdde276
aathika <83359584+Aathika08@users.noreply.github.com>
2021-07-29 21:57:51 +0530

    Add files via upload

---
+++ Main (9).java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = 3;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
