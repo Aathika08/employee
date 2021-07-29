9706096b7192706fe2515ea42ea832325a6f9a3b
aathika <83359584+Aathika08@users.noreply.github.com>
2021-07-29 21:18:44 +0530

    Add files via upload

---
+++ Main (8).java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Name    Yearofjoining    Address");
+		System.out.println("Robert  1994             64C-WallsStreet");
+		System.out.println("Sam     2000             64D-WallsStreet");
+		System.out.println("John    1999             26B-WallsStreet");
+	}
+}
